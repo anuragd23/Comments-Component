81dc16a4710abbb0559f68b447703151d9dee945
Anurag Das <anurag.das23.ad@gmail.com>
2021-01-02 20:40:58 -0500

    Passed custom component through props

--- src/index.js
+++ src/index.js
@@ -7,25 +7,30 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
     return (
         <div className="ui container comments">
-            <ApprovalCard/>
-            <CommentDetail 
-            author="Sam" 
-            timeAgo="Today at 4:45PM" 
-            avatar={faker.image.image()} 
-            content="Nice blog post" 
-            />
-            <CommentDetail 
-            author="Alex" 
-            timeAgo="Today at 2:00AM" 
-            avatar={faker.image.image()} 
-            content="I like the subject" 
-            />
-            <CommentDetail 
-            author="Jane" 
-            timeAgo="Yesterday at 5:00PM" 
-            avatar={faker.image.image()} 
-            content="I like the writing" 
-            />
+            <ApprovalCard>
+              <CommentDetail 
+                author="Sam" 
+                timeAgo="Today at 4:45PM" 
+                avatar={faker.image.image()} 
+                content="Nice blog post" 
+              />
+            </ApprovalCard>
+            <ApprovalCard>
+              <CommentDetail 
+                author="Alex" 
+                timeAgo="Today at 2:00AM" 
+                avatar={faker.image.image()} 
+                content="I like the subject" 
+              />
+            </ApprovalCard>
+            <ApprovalCard>
+              <CommentDetail 
+                author="Jane" 
+                timeAgo="Yesterday at 5:00PM" 
+                avatar={faker.image.image()} 
+                content="I like the writing" 
+              />
+            </ApprovalCard>
         </div>
     );
 };
