55f4f9da21546c099413108262ab0f3cdc928cd4
Anurag Das <anurag.das23.ad@gmail.com>
2021-01-02 19:23:56 -0500

    Created component ApprovalCard

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
-        <div classNmae="ui container comments">
-            <CommentDetail></CommentDetail>
+        <div className="ui container comments">
+            <ApprovalCard/>
+            <CommentDetail 
+            author="Sam" 
+            timeAgo="Today at 4:45PM" 
+            avatar={faker.image.image()} 
+            content="Nice blog post" 
+            />
+            <CommentDetail 
+            author="Alex" 
+            timeAgo="Today at 2:00AM" 
+            avatar={faker.image.image()} 
+            content="I like the subject" 
+            />
+            <CommentDetail 
+            author="Jane" 
+            timeAgo="Yesterday at 5:00PM" 
+            avatar={faker.image.image()} 
+            content="I like the writing" 
+            />
         </div>
-
     );
 };
 
